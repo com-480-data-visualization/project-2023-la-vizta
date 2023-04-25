658b1bd1f64e44b3fba95ce7dd9057afb8be9039
PeacefulOtter <zankro67@gmail.com>
2023-04-25 09:50:31 +0200

    Fixed Russia and Czechia having the wrong name in the countries table

--- countries/src/insert_country.py
+++ countries/src/insert_country.py
@@ -4,10 +4,13 @@ from database import insert_db
 
 relations = pd.read_json('relations_simplified.json')
 centroids = pd.read_csv('centroids_processed.csv')
-
-print(relations)
-print(centroids)
 countries = pd.merge(relations, centroids, on=['iso'])
+
+# Update name to match region name from Spotify Charts
+countries.loc[countries.name == 'Russian Federation', 'name'] = 'Russia'
+countries.loc[countries.name == 'Czech Republic', 'name'] = 'Czechia'
+
+print(countries)
 
 query = """
 INSERT into countries(id, iso, name, lat, lng, geom) VALUES
