3ed1d206899ffd52c6b206603af4c4dbb304dcec
PeacefulOtter <zankro67@gmail.com>
2023-04-25 09:42:33 +0200

    Added GeoJSON onmouseover effect and removed some garbage folders

--- nextjs-frontend/src/components/genres/Country.tsx
+++ nextjs-frontend/src/components/genres/Country.tsx
@@ -1,14 +1,19 @@
 
 import { GeoJSON, useMap } from 'react-leaflet';
+import Color from 'color'
 
 import { GenreName, Track } from '~/types';
 import { paletteGenre } from '~/constants';
+import { log } from 'console';
 
 interface IRegion {
     color: string;
     geom: string;
     onClick: () => void;
 }
+
+const highlight = (color: string) => Color(color).saturate(0.5).lighten(0.2)
+const darken = (color: string) => Color(color).saturate(0.5).darken(0.2)
 
 export default function Country( {color, geom, onClick}: IRegion ) 
 {
@@ -19,9 +24,19 @@ export default function Country( {color, geom, onClick}: IRegion )
         map.flyTo(latlng, map._zoom, {duration: 0.5, noMoveStart: false})
     }
 
+    const onEachFeature = (feature, layer) => {
+        layer.on('mouseover', function(e){
+            layer.setStyle( { ...layer.options.style, fillColor: highlight(color), color: darken(color) } )
+        });
+        layer.on('mouseout', function(e){
+            layer.setStyle( { ...layer.options.style, fillColor: color, color } )
+        });
+    }
+
     return (
         <GeoJSON 
             style={{fillColor: color, color, weight: 1}} 
+            onEachFeature={onEachFeature}
             data={JSON.parse(geom)} 
             eventHandlers={{click: _onClick}} />
     )
